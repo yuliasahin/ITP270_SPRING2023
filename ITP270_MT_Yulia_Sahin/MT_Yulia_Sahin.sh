b40255df4bc3f58abd81b61c6bf87ea8d4dbbbdc
Cyber Range Student <student@ip-10-1-164-208.ec2.internal>
2023-03-10 01:28:46 +0000

    All files to repository

---
+++ ITP270_MT_Yulia_Sahin/MT_Yulia_Sahin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ read -p "Enter 'google.com' to ping, anything else is invalid"
+
+ #The input validation 
+ if [ "$hostname" = "google.com" ]; then 
+         echo "The input must be google.com, anything else is invalid" 
+ exit 1 
+ fi
+
+ #added results 
+ #ping -c 5 "$hostname" > ping_results.txt 
+ #  Print a message indicating that the ping is complete 
+ echo "Success! Results added to ping_results.txt."
